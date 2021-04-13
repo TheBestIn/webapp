064b891b5d293de691e84bb3c018d98c3272e36b
Mirec <Mirec@DESKTOP-QRBR9A4>
2021-04-13 16:20:46 +0200

    ..

---
+++ webappp/src/main/webapp/LoginPage.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+var name=document.forms['form']['username'];
+
+function cheackData(){
+	
+	if(name.value=="mirko"){
+		alert("Data was correect!");
+		
+	}else{
+		alert("incorrect data!");
+	}
+}

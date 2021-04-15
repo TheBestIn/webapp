6165f09c1fcd794b42984d567c51e147e34b854d
Mirec <Mirec@DESKTOP-QRBR9A4>
2021-04-15 16:30:42 +0200

    ..

--- webappp/src/main/webapp/LoginPage.js
+++ webappp/src/main/webapp/LoginPage.js
@@ -8,27 +8,14 @@
 var name=document.forms['form']['username'];
 var pass=document.forms['form']['password'];
 
-function cheackUsername(){
+function cheackUsernameANDpassword(){
 	
-	if(name.value=="mirko" || pass.value=="mirko0011" ){
-		alert("Data wasss correect!");
-		
-	}else{
-		alert("incorrect data!");
-	}
+	if(name.value!="mirko" || pass.value!="mirko0011"){
+	alert("Username or password is not correct!");
+		}
 }
 
-function cheackPassword(){
-	
-	if(pass.value=="mirko0011" ){
-		alert("Password wass correect!");
-		
-	}else{
-		alert("incorrect password!");
-	}
-	
-}
+
 function call(){
-	cheackUsername();
-	cheackPassword();
+cheackUsernameANDpassword();
 }
